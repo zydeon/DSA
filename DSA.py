317b414f1d98665e36a18ab24e8aac0b32c4b064
Pedro Matias <zydeon@gmail.com>
2013-11-19 12:18:28 +0100

    is_valid and beginning of the tokens sign, verify, genkey

--- DSA.py
+++ DSA.py
@@ -1,5 +1,11 @@
 from random import getrandbits
+from math import sqrt
+from sys import exit
+from gmpy2 import is_prime   	# miller-rabin primality test
+from gmpy2 import powmod		# (x ** y) mod m
+from sys import stdin
 
+# z^-1 mod a
 def inverse(z,a):
 	if z > 0 and z < a and a > 0:
 		i = a
@@ -19,7 +25,7 @@ def inverse(z,a):
 
 # Per-Message Secret Number generator
 def number_gen(p,q,g):
-	N = (len(bin(p))-2)
+	N = no_bits(p)
 	c = getrandbits(N+64)
 	k = (c % (q-1))+1
 	try:
@@ -28,10 +34,68 @@ def number_gen(p,q,g):
 	except 'Inverse Error':
 		return number_gen(p,q,g)
 
-def sign((p,q,g),z, H, (x, y)):
+def sign((p,q,g), H, (x, y)):
 	k,k_ = number_gen(p,q,g)
-	r = ((g**k)%p)%q
+	r = powmod(g,k,p) % q
 	z = long(H, 16)
 	s = (k_*(z+x*r)) % q
 
 	return (r, s)
+
+def verify((p,q,g), H, y, (r,s)):
+	if 0 < r and r < q and 0 < s and s < q:
+		w = inverse(s, q)
+		z = long(H, 16)
+		u1 = (z*w) % q
+		u2 = (r*w) % q
+		v = ((g**u1 * y**u2) % p) % q
+		return v == r
+	raise Exception('Verify Error')
+		
+def is_valid(p,q,g):
+	return  ( is_prime(p) and is_prime(q)
+			and no_bits(p) == 1024 and no_bits(q) == 160
+			and (p-1) % q == 0 and powmod(g,q,p) == 1 and g > 1)
+
+# number of bits
+def no_bits(p):
+	return (len(bin(p))-2)
+
+def range_(begin, stop):
+   i = begin
+   while i < stop:
+       yield i
+       i += 1
+
+def group(list, n):
+	return zip(* [list[i::n] for i in range(n)])
+
+if __name__=='__main__':
+
+	p = long(raw_input()[2:])
+	q = long(raw_input()[2:])
+	g = long(raw_input()[2:])
+
+	if not is_valid(p,q,g):
+		print 'invalid_group'
+		exit(-1)
+
+	print 'valid_group'
+
+	token = raw_input()
+	if token == 'genkey':
+		print 'b'
+
+	elif token == 'sign':
+		x = long(raw_input()[2:])
+		y = long(raw_input()[2:])
+		Ds = [l[2:-1] for l in stdin]
+		signs = [sign((p,q,g), d, (x,y)) for d in Ds]
+		for (r,s) in signs:
+			print 'r='+str(r)
+			print 's='+str(s)
+
+	elif token == 'verify':
+		y = long(raw_input()[2:])
+		tuples = group([l[2:-1] for l in stdin], 3)  # tuples (D, r, s)
+		verifies = [verify((p,q,g), t[0], y, (long(t[1]), long(t[2]))) for t in tuples]
